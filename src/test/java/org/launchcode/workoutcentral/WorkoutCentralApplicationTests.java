5f582b68f17757f772dab5bdc59978c02d23a884
BiedeTsion <biede.tsion@gmail.com>
2019-10-24 19:45:04 -0500

    first commit

---
+++ src/test/java/org/launchcode/workoutcentral/WorkoutCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.workoutcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
